3c9de3e2584856f341121dd17638690f8f794ba1
Austin Keller <45545037+kell1353@users.noreply.github.com>
2019-07-24 10:57:03 -0700

    Update wire test.py

--- wire test.py
+++ wire test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sympy.solvers import solve
+from sympy import Symbol
+import math
 
 fig = plt.figure(figsize=(5,5))
 ax = fig.add_subplot(111, projection='3d')
@@ -55,19 +58,46 @@ plt.plot(x3, y3, z3, linewidth=.75, color = 'black')
 ##    ax.scatter(x, y, z)
 ##p(0, 0, 0, 1, 60)
 
-"""Line of line segments"""
-def L(x0, y0, z0, x1, y1, z1, n):                                        # n - number of  dl line segments
-    t = np.linspace(0, 1, n)
-    x = x0 + (x1 - x0)*t
-    y = y0 + (y1 - y0)*t
-    z = z0 + (z1 - z0)*t
-    plt.plot(x, y, z, linewidth=.75, color = 'orange')
-    ax.scatter(x, y, z)
-L(0, 0, 0, 1, 1, 0, 3)
-L(0, 0, 0, 1, 1, 1, 3)
+def draw_field(x0, y0, z0, u, v, w):
+    #mlab.quiver3d(x0, y0, z0, u, v, w, line_width = 1, scale_factor= 2)
+    plt.quiver(x0, y0, z0, u, v, w)
 
+t = Symbol('t')
+def calcDist(x0, y0, z0, x1, y1, z1, xp, yp, zp): #(inital point on line, end point on line, comarison point)
+    global dist
+    xl = x0 + (x1 - x0)*t 
+    yl = y0 + (y1 - y0)*t
+    zl = z0 + (z1 - z0)*t
+    a = [x1 - x0, y1 - y0, z1 - z0]
+
+    #vector from point to arbitrary point on a line
+    vx, vy, vz =  xl - xp, yl - yp, zl - zp
+    b = [vx, vy, vz]
+
+    #Solve for t
+    sol = solve((a[0]*b[0] + a[1]*b[1] + a[2]*b[2]), t)
+
+    #closest point on line
+    xpf = xl.replace(t, float(sol[0]))
+    ypf = yl.replace(t, float(sol[0]))
+    zpf = zl.replace(t, float(sol[0]))
+    pf = (xpf, ypf, zpf)
+
+    #final vector
+    xf = vx.replace(t, float(sol[0]))
+    yf = vy.replace(t, float(sol[0]))
+    zf = vz.replace(t, float(sol[0]))
+    vf = (xf, yf, zf)
+    
+    dist = math.sqrt(xf**2 + yf**2 + zf**2)
+    return dist
+
+
+
+#https://sites.google.com/site/glennmurray/Home/rotation-matrices-and-formulas/rotation-about-an-arbitrary-axis-in-3-dimensions
 """Rotation of a point around an arbitrary axis"""
 def rotate(x, y, z, a, b, c, d, e, f, theta):
+    global xr, yr, zr    
     """Direrction vector"""
     u = d - a
     v = e - b
@@ -75,25 +105,74 @@ def rotate(x, y, z, a, b, c, d, e, f, theta):
 
     L = u**2 + v**2 + w**2
     
-    x1 = (((a*(v**2 + w**2) - u*(b*v + c*w - u*x - v*y - w*z))*(1 - cos(theta)) + L*x*cos(theta) + sqrt(L)*(-c*v + b*w - w*y + v*z)*sin(theta)))/L
-    y1 = (((b*(u**2 + w**2) - v*(a*u + c*w - u*x - v*y - w*z))*(1 - cos(theta)) + L*y*cos(theta) + sqrt(L)*(c*u - a*w + w*x - u*z)*sin(theta)))/L
-    z1 = (((c*(u**2 + v**2) - w*(a*u + b*v - u*x - v*y - w*z))*(1 - cos(theta)) + L*z*cos(theta) + sqrt(L)*(-b*u + a*v - v*x + u*y)*sin(theta)))/L
+    xr = (((a*(v**2 + w**2) - u*(b*v + c*w - u*x - v*y - w*z))*(1 - cos(theta)) + L*x*cos(theta) + sqrt(L)*(-c*v + b*w - w*y + v*z)*sin(theta)))/L
+    yr = (((b*(u**2 + w**2) - v*(a*u + c*w - u*x - v*y - w*z))*(1 - cos(theta)) + L*y*cos(theta) + sqrt(L)*(c*u - a*w + w*x - u*z)*sin(theta)))/L
+    zr = (((c*(u**2 + v**2) - w*(a*u + b*v - u*x - v*y - w*z))*(1 - cos(theta)) + L*z*cos(theta) + sqrt(L)*(-b*u + a*v - v*x + u*y)*sin(theta)))/L
     ax.scatter(x, y, z)
-    ax.scatter(x1, y1, z1)
+    ax.scatter(xr, yr, zr)
+    return xr, yr, zr
 
-##    print(x1)
-##    print(y1)
-##    print(z1)
+
+"""Line of line segments"""
+    #(x -x0/(x1-x0)) = (y - y0/(y1 - y0)) = (z - z0/(z1 - z0))
+def L(x0, y0, z0, x1, y1, z1, n):                                        # n - number of  dl line segments
+    t = np.linspace(0, 1, n)
+    x = x0 + (x1 - x0)*t 
+    y = y0 + (y1 - y0)*t
+    z = z0 + (z1 - z0)*t
+
+    plt.plot(x, y, z, linewidth=.75, color = 'orange')
+    ax.scatter(x, y, z)
+    
+
+"""Line of paralell line segments"""
+def rL(x0, y0, z0, x1, y1, z1, n):                                        # n - number of  dl line segments
+    t = np.linspace(0, 1, n)
+    x = x0 + (x1 - x0)*t 
+    y = y0 + (y1 - y0)*t
+    z = z0 + (z1 - z0)*t
+
+    #plt.plot(x, y, z, linewidth=.75, color = 'orange')
+    #ax.scatter(x, y, z)
+    vx, vy, vz = (xL1 - xL0), (yL1 - yL0), (zL1 - zL0)
+    
+    for i in range(0, 1):
+        t = np.linspace(0, 2*pi, n)
+        for angles in t:
+            rotate(x[i], y[i], z[i], xL0, yL0, zL0, xL1, yL1, zL1, angles)
+            calcDist(xL0, yL0, zL0, xL1, yL1, zL1, xr, yr, zr)
+            
+            ux, uy, uz = -xr, -yr, -zr
+            #cross product of the two vectors
+            bx = uy*vz - uz*vy
+            by = uz*vx - ux*vz
+            bz = ux*vy - uy*vx
+
+            I, u_0 = 1, 1
+            mag = I*u_0/((2*np.pi)*dist)
+            scalingFactor = 2*r
+
+            m = math.sqrt(bx**2 + by**2 + bz**2)
+            draw_field(xr, yr, zr, (mag*bx)/(2*r), (mag*by)/(2*r), (mag*bz)/(2*r))
+
+
+##L(0, 0, 0, 1, 1, 0, 3)
+xL0, yL0, zL0, xL1, yL1, zL1 = 0, 0, 0, 1, 1, 0
+L(xL0, yL0, zL0, xL1, yL1, zL1, 4)
+
+#rL(xL0-r, yL0+r, zL0, xL1-r, yL1+r, zL1, 9)
+for r in np.linspace(.5, 1, 5):
+    rL(xL0-r, yL0+r, zL0, xL1-r, yL1+r, zL1, 9)
+
+
+
+"""Parallel Lines"""
+##r = .2
+##L(0-r, 0+r, 0, 1-r, 1+r, 1, 3)
 ##
-##    print(L)
-##    print(sqrt(L))
-t = np.linspace(0, 2*pi, 25)
-#rotate(1, 0, 0, 0, -1, 0, 0, 1, 0, pi/4)
-#rotate(0, 1, 0, 0, 0, 0, 1/sqrt(3), 1/sqrt(3), 1/sqrt(3), 4.1887902047863905)
-
-for angles in t:
-    rotate(0, 1, 0, 0, 0, 0, 1, 1, 1, angles)
-    #rotate(0, 2, 0, 0, 0, 0, 1, 1, 1, angles)
+##lis = [-.2, .3, .8]
+##for i in lis:
+##        rotate(x[i], y[i], z[i], 0, 0, 0, 1, 1, 1)
 
 
 """Original Line"""
