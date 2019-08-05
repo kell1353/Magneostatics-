7f5ad18685eb7c901c8bd8902df12fcca7b16aef
Austin Keller <45545037+kell1353@users.noreply.github.com>
2019-08-05 13:21:44 -0700

    Update Magnetostatics.py

--- Magnetostatics.py
+++ Magnetostatics.py
@@ -19,12 +19,12 @@ x_axis = mlab.plot3d(0*axes, 0*axes, axes, color=(0,0,0), tube_radius = .02)
 y_axis = mlab.plot3d(axes, 0*axes, 0*axes, color=(0,0,0), tube_radius = .02)
 z_axis = mlab.plot3d(0*axes, axes, 0*axes, color=(0,0,0), tube_radius = .02)
 
+""" Draw 3D points at a given point """
 points_range = 50
 phi = np.linspace(0, 2*np.pi, points_range)
 theta = np.linspace(0, 2*np.pi, points_range)
 phi, theta = np.meshgrid(phi, theta)
 
-""" Draw 3D points at a given point """
 def draw_sphere(x0, y0, z0, r):
     x = (r*sin(phi) * cos(theta)) + x0
     y = (r*sin(phi) * sin(theta)) + y0
@@ -53,29 +53,31 @@ def calcDist(x0, y0, z0, x1, y1, z1, xp, yp, zp): #(inital point on line, end po
     global dist, ux, uy, uz, bx0, by0, bz0, b0_mag
     ux, uy, uz = x1 - x0, y1 - y0, z1 - z0
     a = [ux, uy, uz]
-##    print(ux, uy, uz)
     vx, vy, vz = xp - x0, yp - y0, zp - z0
     wx, wy, wz = xp - x1, yp - y1, zp - z1
+##    print(ux, uy, uz)
 ##    print(vx, vy, vz)
 ##    print("\nPOINTS")
 ##    print(xp, yp, zp)
 
-##    print("\nCROSS PRODUCT")
     bx0 = vy*wz - vz*wy
-    #print(bx0)
     by0 = vz*wx - vx*wz
+    bz0 = vx*wy - vy*wx
+##    print("\nCROSS PRODUCT")
+##    print("\nBX0")
+##    print(bx0)
 ##    print("\nBY0")
 ##    print(by0)
-    bz0 = vx*wy - vy*wx
-    #print(bz0)
-
+##    print("\nBZ0")
+##    print(bz0)
     """Magnitude of Initial Vector"""
     uMag = vectorMag(ux, uy, uz) 
     """Magnitude of Resulting Vector"""
     b0_mag = vectorMag(bx0, by0, bz0)
 
-##    print("\nDISTANCE") ######## Distance should only works for lines a radial distance within the endpoints
+
     dist = b0_mag/uMag
+##    print("\nDISTANCE") ######## Distance should only works for lines a radial distance within the endpoints
 ##    print(dist)
     return dist
 
@@ -97,7 +99,7 @@ def B(I, x, y, z, x0, y0, z0, xf, yf, zf):                                      
     global bx; global by; global bz
     """Magnetic constant"""
     u_0 = 1 
-    #m_u = 1.25663706 * (10**(-6))                                         # m*kg/((s^2)*(A^2))
+    #u_0 = 1.25663706 * (10**(-6))                                         # m*kg/((s^2)*(A^2))
     
     """Nearest distance to the current carrying wire"""
     calcDist(x0, y0, z0, xf, yf, zf, x, y, z)
@@ -109,12 +111,12 @@ def B(I, x, y, z, x0, y0, z0, xf, yf, zf):                                      
     
     """Calculating the vector components for each point in the grid"""
     bx = I*u_0*bx1/(dist*(2*pi))
+    by = I*u_0*by1/(dist*(2*pi))
+    bz =  I*u_0*bz1/(dist*(2*pi))
 ##    print("\nBX")
 ##    print(bx)
-    by = I*u_0*by1/(dist*(2*pi))
 ##    print("\nBY")
 ##    print(by)
-    bz =  I*u_0*bz1/(dist*(2*pi))
 ##    print("\nBZ")
 ##    print(bz)
     return bx,by,bz
